f4b86544980d51a20cdf9354bdd129db5f9513f5
tylerasa <sarpongasaresylvester@gmail.com>
2022-01-29 01:41:43 +0000

    + custom font, Inter

---
+++ src/components/QuestionCard.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./QuestionCard.css";
+const QuestionCard = () => {
+  return (
+    <div className="question-card">
+      <p className="enter-text">Enter Your Question Here</p>
+    </div>
+  );
+};
+
+export default QuestionCard;
---
+++ src/pages/AdminQuestion.js
@@ -0,0 +1,10 @@
+import React from "react";
+import QuestionCard from "../components/QuestionCard";
+import "./AdminQuestions.css";
+export default function AdminQuestion() {
+  return (
+    <div className="container">
+        <QuestionCard/>
+    </div>
+  );
+}
